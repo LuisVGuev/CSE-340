b33524a84592256fc625a7d5dfa914b5c227d8af
mtrackingtechnologies <tag.interaction.web@gmail.com>
2025-10-04 21:42:58 -0300

    main

--- database/index.js
+++ database/index.js
@@ -14,8 +14,6 @@ if (process.env.NODE_ENV == "development") {
       rejectUnauthorized: false,
     },
 })
-
-// Added for troubleshooting queries
 // during development
 module.exports = {
   async query(text, params) {
